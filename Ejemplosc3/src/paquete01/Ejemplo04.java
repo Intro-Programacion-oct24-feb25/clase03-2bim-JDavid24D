8fa73db846ca95935ff899fbf9979e97c4fcad27
JDavid <jorgedaviddiaz62@gmail.com>
2024-12-18 11:54:10 -0500

    Ejemplos

--- Ejemplosc3/src/paquete01/Ejemplo04.java
+++ Ejemplosc3/src/paquete01/Ejemplo04.java
@@ -28,6 +28,7 @@ public class Ejemplo04 {
         };
         
         int suma;
+        int sumaTotal = 0;
         
         for (int fila = 0; fila < arreglo1.length; fila++) { // 0<2 // 1<2
             suma = 0;
@@ -37,11 +38,13 @@ public class Ejemplo04 {
                      // 0 + arreglo1[0][0] // 0 + 10 // 10
                      // 10 + arreglo1[0][1] // 10 + 20 // 30
                      // 30 + arreglo1[0][2] // 30 + 40 // 70
+                
             }
-            System.out.printf("Suma de valores del arreglo: %d\n", suma);
+            sumaTotal = sumaTotal + suma;
+            System.out.printf("Suma de valores del arreglo: %d\n", suma); 
         }
         
-        // System.out.printf("Suma de valores del arreglo: %d\n", suma);
+         System.out.printf("Suma total : %s\n", sumaTotal);
     }
     
 }
